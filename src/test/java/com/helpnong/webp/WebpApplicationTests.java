07f73ef1f0f74ed89125658d2800358777a3d744
trackcrash <auroknh@gmail.com>
2023-05-25 16:25:37 +0900

    initial commit
    first commit

---
+++ src/test/java/com/helpnong/webp/WebpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpnong.webp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
